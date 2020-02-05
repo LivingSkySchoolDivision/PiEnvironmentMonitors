526fc79f3174a58436a93ed185e792bf6fbabe0d
Mark Strendin <mark.strendin@lskysd.ca>
2020-02-05 15:24:53 -0600

    New script for new non-deprecated sensor library

--- Experimental/GenTempJSON.py
+++ Experimental/GenTempJSON.py
@@ -1,11 +1,16 @@
-import Adafruit_DHT
 import datetime
+import board
+import adafruit_dht
+import socket
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 14
+dhtDevice = adafruit_dht.DHT11(board.D14)
 
-humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+humidity = dhtDevice.humidity
+temperature = dhtDevice.temperature
+
 print("{");
+print("\"Hostname\": \"" + str(socket.gethostname()) + "\",")
+
 if temperature is not None:
  print(" \"TemperatureCelsius\": {0:0.1f},".format(temperature));
 else:
