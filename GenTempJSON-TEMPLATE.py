a6954553d6c5c2883abeeb07a9e09c62e2c21fd7
Mark Strendin <mark.strendin@lskysd.ca>
2021-07-21 15:59:11 -0600

    Update GenTempJSON-TEMPLATE.py

--- GenTempJSON-TEMPLATE.py
+++ GenTempJSON-TEMPLATE.py
@@ -9,21 +9,49 @@ import gpiozero
 # To add more sensors, copy the line that starts and ends with { and } respectively
 # Example:
 #  sensors = [
-#       { "Name" : "Sensor One", "Sensor" : adafruit_dht.DHT22(board.D14) },
-#       { "Name" : "Sensor Two", "Sensor" : adafruit_dht.DHT22(board.D15) },
-#       { "Name" : "Sensor Three", "Sensor" : adafruit_dht.DHT11(board.D16) },
+#       { "Name" : "Sensor One", "Description" : "Adafruit DHT22", "Sensor" : adafruit_dht.DHT22(board.D14) },
+#       { "Name" : "Sensor Two", "Description" : "Adafruit DHT22", "Sensor" : adafruit_dht.DHT22(board.D15) },
+#       { "Name" : "Sensor Three", "Description" : "Adafruit DHT11", "Sensor" : adafruit_dht.DHT11(board.D16) },
 #    ]
 #
 # This script supports multiple sensors, that can each be either a DHT11 or a DHT22
 
 sensors = [
-        { "Name" : "Sensor-001", "Sensor" : adafruit_dht.DHT22(board.D14) },
-  ]
+        { "Name" : "S001", "Description" : "Adafruit DHT22", "Sensor" : adafruit_dht.DHT22(board.D14) },
+]
 
 
 # #################################################################
 # You shouldn't need to customize anything below this line
 # #################################################################
+
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
+def getModel():
+  # Extract serial from cpuinfo file
+  returnme = "Unknown"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:5]=='Model':
+        returnme = line[8:999]
+    f.close()
+  except:
+    returnme = "ERROR"
+
+  return returnme.strip()
 
 # Set default values for variables
 # These are what are set if the rest of the script is unable to set them
@@ -32,6 +60,8 @@ errmessage = ""
 cputemp = -999
 uptime = -999
 lastscan = datetime.datetime.now(datetime.timezone.utc)
+model = "Unknown"
+serial = "Unknown"
 
 # Gather data from everything except the temperature sensors (we'll do that next)
 try:
@@ -39,6 +69,8 @@ try:
   cputemp = gpiozero.CPUTemperature().temperature
   with open('/proc/uptime', 'r') as f:
     uptime = float(f.readline().split()[0])
+  serial = getserial()
+  model = getModel()
 except Exception as e:
   errmessage = str(e)
 
@@ -62,22 +94,53 @@ for sensor in sensors:
 
 print("{");
 print(" \"System\": {")
-print(" \"Hostname\": \"" + hostname + "\",")
-print(" \"CPUTempCelsius\": " + str(cputemp) + ",")
-print(" \"UptimeSeconds\": " + str(uptime) + "")
+print("  \"Name\": \"" + hostname + "\",")
+print("  \"Description\": \"" + hostname + "\",")
+print("  \"Model\": \"" + model + "\",")
+print("  \"Serial\": \"" + serial + "\",")
+print("  \"UptimeSeconds\": " + str(uptime) + "")
 print(" },")
-print(" \"Sensors\": [")
 
+print(" \"CPUSensorReading\" : { ");
+print("  \"SystemId\" : \"" + hostname + "\",");
+print("  \"SensorId\" : \"" + hostname + "-cpu\",");
+print("  \"TemperatureCelsius\" : " + str(cputemp) + ",");
+print("  \"ReadingTimestamp\" : \"" + str(lastscan) + "\"");
+print(" },");
+
+print(" \"ExternalSensors\" : [");
 for sensor in sensors:
-  print(" {")
-  print(" \"Name\": \"" + str(sensor["Name"]) + "\",")
-  print(" \"TemperatureCelsius\": {0:0.1f},".format(sensor["Temperature"]))
-  print(" \"Humidity\": {0:0.1f},".format(sensor["Humidity"]))
-  print(" \"ErrorMessage\": \"" + str(sensor["ErrorMessage"]) + "\",")
-  print(" \"IsError\": " + str(sensor["IsError"]))
-  print(" }")
+  print("  {")
+  print("   \"SystemId\" : \"" + hostname + "\",");
+  print("   \"SensorId\" : \"" + hostname + "-" + str(sensor["Name"]) + "\",");
+  print("   \"Description\" : \"" + str(sensor["Description"]) + "\"");
+  print("  }")
+print(" ],")
+
+print(" \"TemperatureReadings\": [")
+for sensor in sensors:
+  if sensor["Temperature"] > -999:
+    print("  {")
+    print("   \"SystemId\" : \"" + hostname + "\",");
+    print("   \"SensorId\" : \"" + hostname + "-" + str(sensor["Name"]) + "\",");
+    print("   \"TemperatureCelsius\": {0:0.1f},".format(sensor["Temperature"]))
+    print("   \"ReadingTimestamp\" : \"" + str(lastscan) + "\"");
+    print("  }")
 
 print(" ],")
+
+print(" \"HumidityReadings\": [")
+for sensor in sensors:
+  if sensor["Humidity"] > -999:
+    print("  {")
+    print("   \"SystemId\" : \"" + hostname + "\",");
+    print("   \"SensorId\" : \"" + hostname + "-" + str(sensor["Name"]) + "\",");
+    print("   \"HumidityPercent\": {0:0.1f},".format(sensor["Humidity"]))
+    print("   \"ReadingTimestamp\" : \"" + str(lastscan) + "\"");
+    print("  }")
+
+print(" ],")
+
 print(" \"Troubleshooting\" : {")
 print("  \"LastScan\": \"" + str(lastscan) + "\",")
 print("  \"Errormessage\": \"" + str(errmessage) + "\"")
